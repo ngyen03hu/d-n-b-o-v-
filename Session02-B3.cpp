6e5d085de14f0735e74128d155b6e58b02372b4b
ngyen03hu <nguyen33hu@gmail.com>
2024-11-19 17:51:54 +0700

    Add files via upload

---
+++ Session02-B3.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+	int a = 20;
+	int b = 5;
+	
+	// toan tu -
+	int hieu = a - b;
+	printf("phep tong la %d\n",hieu);
+	 
+	// toan tu * 
+	int tich = a * b;
+	printf("phep tong là %d\n",tich);
+	
+	// phep chia  / 
+	int thuong = a  / b;
+	printf("phep tong la %d\n",thuong); 
+	 
+	return 0; 
+} 
---
+++ Session02-B4.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+	int a = 5; // do dai mot canh cua hinh vuong
+	
+	// tinh chu vi  cua hinh vuong
+	// cong thuc cua chu vi hinh vuong P=4*a 
+	int P = 4 * a;
+	printf(" chu vi  cua hinh vuong la %d\n",P);
+	
+	
+	// tinh dien tich  hinh vuong
+    // cong thuc tinh dien tich c?a hinh vuong S=a*
+	int S = a * a;
+	printf("dien tich cua hinh vuong la %d\n",S);  
+	
+	
+	 
+	return 0;
+} 
